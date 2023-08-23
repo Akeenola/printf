ec27291e7fe6e3f9487a645d91e02f2069a5b8e3
Tommy Iverson Johnson <75326197+tommyiversonj@users.noreply.github.com>
2023-08-23 01:23:14 +0000

    Update main.h

--- main.h
+++ main.h
@@ -5,7 +5,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 
-int int_printf(int num);
+void printNumbers(int count, ...);
 int string_printf(char *string);
 int char_printf(char achar);
 int _printf(const char *format, ...);
